2df6ca1a40dd33787bde27ef5d7371c110216dde
Steven Black <steveb@stevenblack.com>
2016-10-20 22:48:06 -0400

    More unit tests.

--- test.js
+++ test.js
@@ -11,3 +11,16 @@ test('Create anchor', t => {
   const a1 = new hookModule.Anchor({});
   t.is(a1.isHook(a1), true, 'Anchor is a hook.');
 });
+
+test('Set hook', t => {
+  const h1 = new hookModule.Hook({});
+  const a1 = new hookModule.Anchor({});
+  a1.setHook(h1);
+  t.is(a1.isHook(a1.hook), true, 'Anchor\'s hook is a hook.');
+});
+
+test('Set hook returns a hook', t => {
+  const h1 = new hookModule.Hook({});
+  const a1 = new hookModule.Anchor({});
+  t.is(a1.isHook(a1.setHook(h1)), true, 'setHook() returns a hook.');
+});
