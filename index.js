df69399f3bd25ca1a9f2e947cfefc812b972ad97
Steven Black <steveb@stevenblack.com>
2016-10-20 22:07:43 -0400

    First hool and anchor implementations.

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 'use strict';
-module.exports = (input, opts) => {
 
+class Hook {
+  constructor(options = {}) {
+    this.defaults = {};
+    this.hook = undefined;
+    this.settings = options;
+  }
+  execute() {}
+  process(options) {
+    if (this.preProcess(options)) {
+      this.execute(options);
+      this.postProcess(options);
+    }
+  }
+  preProcess() {}
+  postProcess() {}
+  setHook(hook) {
+    if (this.isHook(this.hook)) {
+      this.hook.setHook(hook);
+    } else {
+      this.hook = hook;
+    }
+    return hook;
+  }
+
+  isHook(hook) {
+    const type = typeof hook;
+    return hook !== null && (type === 'object' || type === 'function') && ('hook' in hook);
+  }
+}
+
+class Anchor extends Hook {
+  constructor(options) {
+    super(options);
+    this.hooks = [];
+  }
+}
+
+module.exports = {
+  Hook,
+  Anchor
 };
