54e701b71ef3bbff943a935c5270f584b8ddc4d8
Steven Black <steveb@stevenblack.com>
2016-10-20 22:08:20 -0400

    First tests.

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 import test from 'ava';
-import fn from './';
 
-test('title', t => {
+const hookModule = require('./');
 
+test('Create hook', t => {
+  const h1 = new hookModule.Hook({});
+  t.is(h1.isHook(h1), true, 'Hook is a hook.');
+});
+
+test('Create anchor', t => {
+  const a1 = new hookModule.Anchor({});
+  t.is(a1.isHook(a1), true, 'Anchor is a hook.');
 });
