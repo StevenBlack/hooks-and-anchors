470c616cf598fb14842d8326d6fe416952b79d8a
Steven Black <steveb@stevenblack.com>
2016-10-27 23:40:07 -0400

    Add a DelayedHook test class.

--- test/common.js
+++ test/common.js
@@ -13,6 +13,42 @@ class TallyHook extends hookModule.Hook {
   }
 }
 
+class DelayableHook extends hookModule.Hook {
+  constructor(options) {
+    super(options)
+  }
+
+  preProcess() {
+    this.delay( this.settings.preprocess || 500, "preProcess");
+    return true
+  }
+
+  execute() {
+    this.delay(this.settings.execute || 200, "execute");
+  }
+
+  postProcess() {
+    this.delay(this.settings.postprocess || 100, "postProcess");
+  }
+
+
+  delay(ms, str){
+    var ctr, rej, p = new Promise((resolve, reject) => {
+        ctr = setTimeout(() => {
+          console.log( `delayed ${str} by ${ms}ms`)
+          resolve();
+          }, ms);
+        rej = reject;
+    });
+
+
+    p.cancel = function(){ clearTimeout(ctr); rej(Error("Cancelled"))};
+    return p;
+  }
+}
+
+
 module.exports = {
-  TallyHook
+  TallyHook,
+  DelayableHook
 };
