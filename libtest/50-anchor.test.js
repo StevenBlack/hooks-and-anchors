cbbc086b79f5ec99c0cd312c6b8e63aef0d437e8
Steven Black <steveb@stevenblack.com>
2016-11-02 11:39:01 -0400

    New generated code.

--- libtest/50-anchor.test.js
+++ libtest/50-anchor.test.js
@@ -4,6 +4,7 @@ var assert = require('chai').assert;
 var expect = require('chai').expect;
 var should = require('should');
 var Anchor = require('../lib/').Anchor;
+var Hook = require('../lib/').Hook;
 var TallyHook = require('./common.js').TallyHook;
 
 describe('Anchor standalone functionality', function () {
@@ -23,6 +24,39 @@ describe('Anchor standalone functionality', function () {
     flags.should.have.property('execute', true);
     flags.should.have.property('hook', true);
     flags.should.have.property('postProcess', true);
+  });
+
+  it('Anchor.setFlags() cascades', function () {
+    var anchor = new Anchor();
+    var hook = new Hook();
+    var hook2 = new Hook();
+    var hook3 = new Hook();
+
+    anchor.setHook(hook).setHook(hook2);
+    anchor.hooks.push(hook3);
+    console.dir(anchor.hooks);
+    // set all flags false
+    anchor.setFlags(false);
+
+    var flags = anchor.flags;
+    flags.should.have.property('execute', false);
+    flags.should.have.property('hook', false);
+    flags.should.have.property('postProcess', false);
+
+    flags = hook.flags;
+    flags.should.have.property('execute', false);
+    flags.should.have.property('hook', false);
+    flags.should.have.property('postProcess', false);
+
+    flags = hook2.flags;
+    flags.should.have.property('execute', false);
+    flags.should.have.property('hook', false);
+    flags.should.have.property('postProcess', false);
+
+    flags = anchor.hooks[0].flags;
+    flags.should.have.property('execute', false);
+    flags.should.have.property('hook', false);
+    flags.should.have.property('postProcess', false);
   });
 
   it('anchor hook added by setHook method', function () {
