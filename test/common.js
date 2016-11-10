adb212fd725006064a8a710033d9b1d28d091358
Steven Black <steveb@stevenblack.com>
2016-11-09 23:09:01 -0500

    Remove the console.log statements from the test fixture now that we dave debug().

--- test/common.js
+++ test/common.js
@@ -7,19 +7,16 @@ class TallyHook extends Hook {
 
   preProcess(thing, resolve, reject)  {
     debug(`Hook ${this.name} - preProcess()`);
-    console.log(`Hook ${this.name} - preProcess()`);
     setTimeout(this.randomTimeProcess.bind(this, thing, resolve, 'pre') , Math.random()*1000)
   }
 
   execute(thing, resolve, reject)  {
     debug(`Hook ${this.name} - execute()`);
-    console.log(`Hook ${this.name} - execute()`);
     setTimeout(this.randomTimeProcess.bind(this, thing, resolve, 'exe') , Math.random()*1000)
   }
 
   postProcess(thing, resolve, reject) {
     debug(`Hook ${this.name} - postProcess()`);
-    console.log(`Hook ${this.name} - postProcess()`);
     setTimeout(this.randomTimeProcess.bind(this, thing, resolve, 'post') , Math.random()*1000)
   }
 
@@ -54,7 +51,6 @@ class DelayableHook extends Hook {
 
   execute(thing) {
     debug(`Hook ${this.name} - execute()`);
-    console.log(`executing Hook depth ${this.depth}`);
     this.delay(this.settings.execute || 200, "execute");
   }
 
@@ -66,9 +62,7 @@ class DelayableHook extends Hook {
 
   delay(ms, str){
     var ctr, rej, p = new Promise((resolve, reject) => {
-        // console.log( `setting Hook depth ${this.depth} delayed ${str} by ${ms}ms ` + new Date());
         ctr = setTimeout(() => {
-          console.log( `delayed ${str} by ${ms}ms`)
           thing[this.name].push(`Hook ${this.depth} ${str}`)
           done();
           }, ms);
