fef5fffaa40bd5f6791509c623eac70ea69055a5
Steven Black <steveb@stevenblack.com>
2016-11-09 19:34:02 -0500

    Debug statements in test classes.

--- test/common.js
+++ test/common.js
@@ -1,25 +1,32 @@
 'use strict';
 
+const debug = require('debug')('HNA');
 const Hook = require("../lib/").Hook;
 
 class TallyHook extends Hook {
 
   preProcess(thing, resolve, reject)  {
+    debug(`Hook ${this.name} - preProcess()`);
+    console.log(`Hook ${this.name} - preProcess()`);
     setTimeout(this.randomTimeProcess.bind(this, thing, resolve, 'pre') , Math.random()*1000)
   }
 
   execute(thing, resolve, reject)  {
+    debug(`Hook ${this.name} - execute()`);
+    console.log(`Hook ${this.name} - execute()`);
     setTimeout(this.randomTimeProcess.bind(this, thing, resolve, 'exe') , Math.random()*1000)
   }
 
   postProcess(thing, resolve, reject) {
+    debug(`Hook ${this.name} - postProcess()`);
+    console.log(`Hook ${this.name} - postProcess()`);
     setTimeout(this.randomTimeProcess.bind(this, thing, resolve, 'post') , Math.random()*1000)
   }
 
   randomTimeProcess(thing, resolve, stage) {
     thing[stage + 'Tally'] = thing[stage + 'Tally'] || 0;
     thing[stage + 'Tally'] = thing[stage + 'Tally'] + 1;
-    console.log(`${this.name} ${stage} ${thing[stage + 'Tally']}`);
+    debug(`${this.name} ${stage} ${thing[stage + 'Tally']}`);
     resolve(thing);
     return thing;
   }
@@ -39,17 +46,20 @@ class DelayableHook extends Hook {
   }
 
   preProcess(thing) {
+    debug(`Hook ${this.name} - preProcess()`);
     thing[this.name] = thing[this.name] || [];
     this.delay( this.settings.preprocess || 500, "preProcess");
     return true
   }
 
   execute(thing) {
+    debug(`Hook ${this.name} - execute()`);
     console.log(`executing Hook depth ${this.depth}`);
     this.delay(this.settings.execute || 200, "execute");
   }
 
   postProcess(thing) {
+    debug(`Hook ${this.name} - postProcess()`);
     this.delay(this.settings.postprocess || 100, "postProcess");
   }
 
