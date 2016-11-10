3ac077d4f1bae3b200feb15d845302dde7a094b7
Steven Black <steveb@stevenblack.com>
2016-11-09 19:34:42 -0500

    Wrap test packages in a describe() so we know which package they came from.

--- test/10-hook.test.js
+++ test/10-hook.test.js
@@ -4,124 +4,126 @@ const assert = require('chai').assert;
 const expect = require('chai').expect;
 const should = require('should');
 const Hook = require('../lib/').Hook;
-const TallyHook = require('./common.js').TallyHook;
-const DelayableHook = require('./common.js').DelayableHook;
+const TallyHook = require('../libtest/common.js').TallyHook;
+const DelayableHook = require('../libtest/common.js').DelayableHook;
 
-describe('Hook standalone functionality', () => {
-  it('hook default name is "Hook"', () => {
-    const hook = new Hook();
-    hook.should.have.property('name', 'Hook');
-  });
-
-  it('hook has assigned name', () => {
-    const hook = new Hook({name: 'foo'});
-    hook.should.have.property('name', 'foo');
-  });
-
-  it('Hook process flags are all true', () => {
-    const hook = new Hook();
-    const flags = hook.flags;
-    flags.should.have.property('execute', true);
-    flags.should.have.property('hook', true);
-    flags.should.have.property('postProcess', true);
-  });
-
-  it('Hook.setFlags() cascades', () => {
-    const hook = new Hook();
-    const hook2 = new Hook();
-    hook.setHook(hook2);
-    // set all flags false
-    hook.setFlags(false);
-
-    let flags = hook.flags;
-    flags.should.have.property('execute', false);
-    flags.should.have.property('hook', false);
-    flags.should.have.property('postProcess', false);
-
-    flags = hook2.flags;
-    flags.should.have.property('execute', false);
-    flags.should.have.property('hook', false);
-    flags.should.have.property('postProcess', false);
-  });
-
-  it('Hook added by setHook method', () => {
-    const hook = new Hook();
-    const hook2 = new Hook();
-    hook.setHook(hook2);
-    expect(hook.isHook(hook.hook)).to.equal(true);
-  });
-});
-
-describe('Hook functionality test', () => {
-  it('Hook methods all fire', () => {
-    let obj = {
-      preTally: 0,
-      postTally: 0
-    };
-    const hook = new TallyHook();
-    const hookA = new TallyHook();
-    const hookB = new TallyHook();
-    hook.setHook(hookA).setHook(hookB);
-    const promise = hook.process(obj);
-    promise.should.be.a.Promise();
-
-    promise.then((obj) => {
-      obj.should.have.property('preTally', 3)
+describe(`Tests in ${__filename}`, () => {
+  describe('Hook standalone functionality', () => {
+    it('hook default name is "Hook"', () => {
+      const hook = new Hook();
+      hook.should.have.property('name', 'Hook');
     });
 
-    promise.then((obj) => {
-      obj.should.have.property('postTally', 3)
+    it(`hook has assigned name 'foo'`, () => {
+      const hook = new Hook({name: 'foo'});
+      hook.should.have.property('name', 'foo');
     });
 
-    promise.catch((reason) => {console.dir(reason);} )
+    it('Hook process flags are all true', () => {
+      const hook = new Hook();
+      const flags = hook.flags;
+      flags.should.have.property('execute', true);
+      flags.should.have.property('hook', true);
+      flags.should.have.property('postProcess', true);
+    });
+
+    it('Hook.setFlags() cascades', () => {
+      const hook = new Hook({name:'hook'});
+      const hook2 = new Hook({name:'hook2'});
+      hook.setHook(hook2);
+      // set all flags false
+      hook.setFlags(false);
+
+      let flags = hook.flags;
+      flags.should.have.property('execute', false);
+      flags.should.have.property('hook', false);
+      flags.should.have.property('postProcess', false);
+
+      flags = hook2.flags;
+      flags.should.have.property('execute', false);
+      flags.should.have.property('hook', false);
+      flags.should.have.property('postProcess', false);
+    });
+
+    it('Hook added by setHook method', () => {
+      const hook = new Hook({name:'hook'});
+      const hook2 = new Hook({name:'hook2'});
+      hook.setHook(hook2);
+      expect(hook.isHook(hook.hook)).to.equal(true);
+    });
   });
-});
 
-describe('Class instance from string', () => {
-  it('creates instance as expected', () => {
-    const myString = "../test/myclass";
-    const hook = new Hook();
-    let obj = hook.classInstanceFromString(myString);
-    obj.should.have.property('name', 'MyClass');
-    const temp = require(myString);
-    obj.should.be.instanceof(temp, 'The object should be of the extpected instanceOf.');
-    obj = hook.classInstanceFromString(myString, {'name': 'foo'});
-    obj.should.have.property('name', 'foo');
+  describe('Hook functionality test', () => {
+    it('Hook methods all fire', () => {
+      let obj = {
+        preTally: 0,
+        postTally: 0
+      };
+      const hook = new TallyHook({name:'tallyHook'});
+      const hookA = new TallyHook({name:'tallyHookA'});
+      const hookB = new TallyHook({name:'tallyHookB'});
+      hook.setHook(hookA).setHook(hookB);
+      const promise = hook.process(obj);
+      promise.should.be.a.Promise();
+
+      promise.then((obj) => {
+        obj.should.have.property('preTally', 3)
+      });
+
+      promise.then((obj) => {
+        obj.should.have.property('postTally', 3)
+      });
+
+      promise.catch((reason) => {console.dir(reason);} )
+    });
   });
-});
 
-describe('Passing a class name to setHook()', () => {
-  it('creates instance as expected', () => {
-    const myString = "../test/myhookclass";
-    const hook = new Hook();
-    let obj= hook.setHook(myString)
-    expect(hook.isHook(obj)).to.equal(true);
-    obj.should.have.property('name', 'Hook');
+  describe('Class instance from string', () => {
+    it('creates instance as expected', () => {
+      const myString = "../test/myclass";
+      const hook = new Hook();
+      let obj = hook.classInstanceFromString(myString);
+      obj.should.have.property('name', 'MyClass');
+      const temp = require(myString);
+      obj.should.be.instanceof(temp, 'The object should be of the extpected instanceOf.');
+      obj = hook.classInstanceFromString(myString, {'name': 'foo'});
+      obj.should.have.property('name', 'foo');
+    });
   });
-});
 
-describe('Passing a class name to and options to setHook()', () => {
-  it('creates instance named foo, as expected', () => {
-    const myString = "../test/myhookclass";
-    const hook = new Hook();
-    let obj= hook.setHook(myString, {name:'foo'})
-    expect(hook.isHook(obj)).to.equal(true);
-    obj.should.have.property('name', 'foo');
+  describe('Passing a class name to setHook()', () => {
+    it('creates instance as expected', () => {
+      const myString = "../test/myhookclass";
+      const hook = new Hook();
+      let obj= hook.setHook(myString)
+      expect(hook.isHook(obj)).to.equal(true);
+      obj.should.have.property('name', 'Hook');
+    });
   });
-});
 
-describe('Hook execution sequence', () => {
-  it('is as expected', () => {
-    const hook1 = new DelayableHook();
-    const hook2 = new DelayableHook();
-    hook1.setHook(hook2);
-    let thing = {};
+  describe('Passing a class name to and options to setHook()', () => {
+    it('creates instance named foo, as expected', () => {
+      const myString = "../test/myhookclass";
+      const hook = new Hook();
+      let obj= hook.setHook(myString, {name:'foo'})
+      expect(hook.isHook(obj)).to.equal(true);
+      obj.should.have.property('name', 'foo');
+    });
+  });
 
-    let p = hook1.process(thing);
+  describe('Hook execution sequence', () => {
+    it('is as expected', () => {
+      const hook1 = new DelayableHook();
+      const hook2 = new DelayableHook();
+      hook1.setHook(hook2);
+      let thing = {};
 
-    p.then(() => {console.log('Done!');console.dir(thing);console.log(new Date());})
-    .catch((err) =>{
-      console.dir(err);
+      let p = hook1.process(thing);
+
+      p.then(() => {console.log('Done!');console.dir(thing);console.log(new Date());})
+      .catch((err) =>{
+        console.dir(err);
+      });
     });
   });
 });
--- test/50-anchor.test.js
+++ test/50-anchor.test.js
@@ -5,100 +5,101 @@ const expect = require('chai').expect;
 const should = require('should');
 const Anchor = require('../lib/').Anchor;
 const Hook   = require('../lib/').Hook;
-const TallyHook = require('./common.js').TallyHook;
+const TallyHook = require('../libtest/common.js').TallyHook;
 
-describe('Anchor standalone functionality', function(){
-  it('anchor default name is "Anchor"', function(){
-    const anchor = new Anchor();
-    anchor.should.have.property('name', 'Anchor');
-  });
+describe(`Tests in ${__filename}`, () => {
+  describe('Anchor standalone functionality', function(){
+    it('anchor default name is "Anchor"', function(){
+      const anchor = new Anchor();
+      anchor.should.have.property('name', 'Anchor');
+    });
 
-  it('anchor has assigned name', function(){
-    const anchor = new Anchor({name:'foo'});
-    anchor.should.have.property('name', 'foo');
-  });
+    it('anchor has assigned name', function(){
+      const anchor = new Anchor({name:'foo'});
+      anchor.should.have.property('name', 'foo');
+    });
 
-  it('anchor process flags are all true', function(){
-    const anchor = new Anchor();
-    const flags = anchor.flags;
-    flags.should.have.property('execute', true);
-    flags.should.have.property('hook', true);
-    flags.should.have.property('postProcess', true);
-  });
+    it('anchor process flags are all true', function(){
+      const anchor = new Anchor();
+      const flags = anchor.flags;
+      flags.should.have.property('execute', true);
+      flags.should.have.property('hook', true);
+      flags.should.have.property('postProcess', true);
+    });
 
-  it('Anchor.setFlags() cascades', () => {
-    const anchor = new Anchor();
-    const hook = new Hook();
-    const hook2 = new Hook();
-    const hook3 = new Hook();
+    it('Anchor.setFlags() cascades', () => {
+      const anchor = new Anchor();
+      const hook = new Hook();
+      const hook2 = new Hook();
+      const hook3 = new Hook();
 
-    anchor.setHook(hook).setHook(hook2);
-    anchor.hooks.push(hook3);
+      anchor.setHook(hook).setHook(hook2);
+      anchor.hooks.push(hook3);
 
-    // set all flags false
-    anchor.setFlags(false);
+      // set all flags false
+      anchor.setFlags(false);
 
-    let flags = anchor.flags;
-    flags.should.have.property('execute', false);
-    flags.should.have.property('hook', false);
-    flags.should.have.property('postProcess', false);
-
-
-    flags = hook.flags;
-    flags.should.have.property('execute', false);
-    flags.should.have.property('hook', false);
-    flags.should.have.property('postProcess', false);
-
-    flags = hook2.flags;
-    flags.should.have.property('execute', false);
-    flags.should.have.property('hook', false);
-    flags.should.have.property('postProcess', false);
-
-    flags = anchor.hooks[0].flags;
-    flags.should.have.property('execute', false);
-    flags.should.have.property('hook', false);
-    flags.should.have.property('postProcess', false);
-  });
+      let flags = anchor.flags;
+      flags.should.have.property('execute', false);
+      flags.should.have.property('hook', false);
+      flags.should.have.property('postProcess', false);
 
 
-  it('anchor hook added by setHook method', function(){
-    const anchor = new Anchor();
-    const anchor2 = new Anchor();
-    anchor.setHook(anchor2);
-    expect(anchor.isHook(anchor.hook)).to.equal(true);
-  });
-});
+      flags = hook.flags;
+      flags.should.have.property('execute', false);
+      flags.should.have.property('hook', false);
+      flags.should.have.property('postProcess', false);
 
-describe('Anchor array functionality', function(){
-  it('array hook methods all fire', function(){
-    let testObj = {
-      preTally: 0,
-      exeTally: 0,
-      postTally: 0
-    };
-    const anchor = new Anchor();
-    const hookA = new TallyHook();
-    const hookB = new TallyHook();
-    const hookC = new TallyHook();
-    const hookD = new TallyHook();
-    const hookE = new TallyHook();
+      flags = hook2.flags;
+      flags.should.have.property('execute', false);
+      flags.should.have.property('hook', false);
+      flags.should.have.property('postProcess', false);
 
-    anchor.hooks.push(hookA);
-    anchor.hooks.push(hookB);
-    anchor.hooks.push(hookC);
-    anchor.setHook(hookD);
-    anchor.setHook(hookE);
+      flags = anchor.hooks[0].flags;
+      flags.should.have.property('execute', false);
+      flags.should.have.property('hook', false);
+      flags.should.have.property('postProcess', false);
+    });
 
-    let promise = anchor.process(testObj);
-    promise.should.be.a.Promise();
-    promise.then((testObj) => {
-      should(testObj).have.property('preTally', 5);
-      should(testObj).have.property('exeTally', 5);
-      should(testObj).have.property('postTally', 5);
-    })
-    .catch((err) =>{
-      console.dir(err);
+
+    it('anchor hook added by setHook method', function(){
+      const anchor = new Anchor();
+      const anchor2 = new Anchor();
+      anchor.setHook(anchor2);
+      expect(anchor.isHook(anchor.hook)).to.equal(true);
     });
   });
 
+  describe('Anchor array functionality', function(){
+    it('array hook methods all fire', function(){
+      let testObj = {
+        preTally: 0,
+        exeTally: 0,
+        postTally: 0
+      };
+      const anchor = new Anchor();
+      const hookA = new TallyHook();
+      const hookB = new TallyHook();
+      const hookC = new TallyHook();
+      const hookD = new TallyHook();
+      const hookE = new TallyHook();
+
+      anchor.hooks.push(hookA);
+      anchor.hooks.push(hookB);
+      anchor.hooks.push(hookC);
+      anchor.setHook(hookD);
+      anchor.setHook(hookE);
+
+      let promise = anchor.process(testObj);
+      promise.should.be.a.Promise();
+      promise.then((testObj) => {
+        should(testObj).have.property('preTally', 5);
+        should(testObj).have.property('exeTally', 5);
+        should(testObj).have.property('postTally', 5);
+      })
+      .catch((err) =>{
+        console.dir(err);
+      });
+    });
+  });
 });
